9d8362dbe81593ce66002fe59884175a7622f80f
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2021-12-23 12:57:27 -0400

    add tratando a tag input

--- DIO_TypeScript/index.ts
+++ DIO_TypeScript/index.ts
@@ -1,26 +1,6 @@
-// types
-// interfaces
+const input = document.getElementById('input') as HTMLInputElement
 
-interface IAnimal {
-    nome: string
-    tipo: 'terrestre' | 'aquático'
-    executarRugido(alturaEmDecibeis: number): void
-}
-
-interface IFelino extends IAnimal {
-    visaoNoturna: boolean
-}
-
-const animal: IAnimal = {
-    nome: 'Elefante',
-    tipo: 'terrestre'
-}
-
-const felino: IFelino = {
-    nome: 'Leão',
-    tipo: 'terrestre',
-    visaoNoturna: true
-    executarRugido: (alturaEmDecibeis) => (`${alturaEmDecibeis}db`)
-}
-
-animal.executarRugido('s')
+input.addEventListener('input', event => {
+    const i = event.currentTarget as HTMLInputElement
+    console.log(i)
+})
