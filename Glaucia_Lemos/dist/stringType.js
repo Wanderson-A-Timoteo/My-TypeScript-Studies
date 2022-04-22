341e669fc76271bd54f86ec1fb859e5d22d2e5c7
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2022-04-22 13:53:39 -0400

    Adicionado exemplo de tipo string

---
+++ Glaucia_Lemos/dist/stringType.js
@@ -0,0 +1,12 @@
+"use strict";
+// ==> Exemplo 01 - Single Quotes
+let nomeCompleto = 'Wanderson Timóteo';
+console.log(nomeCompleto);
+// ==> Exemplo 02 - Double Quotes
+let funcaoEmpresa = "Programador de Sistemas";
+console.log(funcaoEmpresa);
+// ==> Exemplo 03 - Back Ticks
+let nomeEmpresa = 'FontData Tecnologia';
+let dadosFuncionario = `Seja bem-vindo ${nomeCompleto}! Você será ${funcaoEmpresa} 
+    na empresa ${nomeEmpresa}`;
+console.log(dadosFuncionario);
---
+++ Glaucia_Lemos/stringType.ts
@@ -0,0 +1,18 @@
+// ==> Exemplo 01 - Single Quotes
+
+let nomeCompleto: string = 'Wanderson Timóteo';
+console.log(nomeCompleto);
+
+// ==> Exemplo 02 - Double Quotes
+
+let funcaoEmpresa: string = "Programador de Sistemas";
+console.log(funcaoEmpresa);
+
+// ==> Exemplo 03 - Back Ticks
+
+let nomeEmpresa: string = 'FontData Tecnologia';
+
+let dadosFuncionario: string = `Seja bem-vindo ${nomeCompleto}! Você será ${funcaoEmpresa} 
+    na empresa ${nomeEmpresa}`; 
+
+console.log(dadosFuncionario);
