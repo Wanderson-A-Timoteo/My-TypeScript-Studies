1d91cd545cba44e05ece42f99fec1864aecbbbe6
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2022-04-29 13:31:43 -0400

    Adicionado exemplo de tipo unknown

---
+++ Glaucia_Lemos/dist/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+"use strict";
+let algumaCoisaAny;
+// let algumaCoisaUnknown: unknown;
+// console.log(algumaCoisaAny.toFixed());
+// if (typeof algumaCoisaUnknown === 'number') {
+//   console.log(algumaCoisaUnknown.toFixed());
+// }
